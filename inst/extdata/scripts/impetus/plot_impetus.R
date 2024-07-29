a2ecf52fade63045ebdc430dfcadd0ee4709265e
ma-z-am <malte.zamzow@kompetenz-wasser.de>
2024-07-29 13:26:50 +0200

    Update plot_impetus.R

--- inst/extdata/scripts/impetus/plot_impetus.R
+++ inst/extdata/scripts/impetus/plot_impetus.R
@@ -10,13 +10,18 @@ t_table <- read.table(
   header = TRUE,
   sep = ";")
 
-# der zweite Teil der Bezeichnung von des Strangs (section) sollte in die Übersetzungstabelle eingehen
+# der zweite Teil der Bezeichnung des Strangs (section) sollte in die Übersetzungstabelle eingehen
 # sonst wird zum Beispiel ein Teil des WHK nicht geplotted, weil er als BSK erkannt wird
-aggregated_data <- qsimVis::qsim_to_verknet_id(
+aggregated_data <- qsimVis::add_qsimVis_id(
   aggregated_data = ag_table,
   translation_table = t_table)
 
-rivers <- qsimVis::load_rivers(aggregated_data = aggregated_data)
+# hier sollte nur "section_name" und "km" ausgegeben werden
+rivers <- qsimVis::load_rivers(
+  aggregated_data = ag_table,
+  translation_table = t_table,
+  path_manual = system.file(package = "qsimVis", "extdata/manually_added_rivers")
+)
 
 rivers_ext <- lapply(
   X = names(rivers), FUN = qsimVis::extend_riverTable,
@@ -47,19 +52,6 @@ qsimVis::add_coloredRivers(
   LegendTitle = "Durchschnittlicher \nAbwassergehalt in %"
 )
 
-# Add CSO volume
-# qsimVis::add_inflow(
-#   filname = file.path(
-#     "C:/Users/mzamzo/Documents/R/git",
-#     "qsimVis/inst/extdata/cso_data",
-#     "E2_2011_20221209_14-52_stats.csv"),
-#   varName = "tVol_m3",
-#   sizeMax = 100000)
 
-
-# Add flow (average during time period or before event)
-
-# Path for saving
-saving_path <- file.path(scenario_path, "5_assessment_output")
 
 
