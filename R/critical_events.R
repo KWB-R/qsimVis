b0299813ab5bae2b345eb5f3b559af28e56862e7
ma-z-am <malte.zamzow@kompetenz-wasser.de>
2025-09-24 13:38:46 +0200

    Add dev_type option for critical events

--- R/critical_events.R
+++ R/critical_events.R
@@ -5,14 +5,17 @@
 #' time above that threshold. Furthermore, the exceedance
 #' of a value can also be a separation criterion.
 #'
-#' @param dataFrame Data frame with time in first column and parameter
-#' values per site.
+#' @param dataFrame Data frame with posixDateTime column and at leaste 2 site
+#' columns
 #' @param deficiency_hours Minimal time period of values below threshold.
 #' @param separating_hours Minimal time period between two values below
 #' threshold to count as separate events.
 #' @param threshold Numeric in the same unit as the data vector
+#' @param dev_type Either "elt" (equal or lower than) or "egt" (equal or higher
+#' than) to find events below or above the threshold value.
 #' @param recovery_value If this is not NULL (default) two events are only
 #' separated if a numerical revocvery value is exceeded between two deficits.
+#' That does not impact the event seperation by time (seperating_hours).
 #' @param return_event_positions Instead the number of events, the events
 #' starting and end positions are returned, corresponding to the data vector
 #'
@@ -34,34 +37,48 @@ critical_events <- function(
     separating_hours,
     deficiency_hours,
     threshold,
+    dev_type = "elt",
     recovery_value = NULL,
     return_event_positions = FALSE
 ){
-  dates <- dataFrame[,1]
+  dates <- dataFrame[["posixDateTime"]]
   resolution <- as.numeric(difftime(
     time1 = dates[2],
     time2 = dates[1],
     units = "hours"))
 
   starting_data_points <- round(1 + deficiency_hours / resolution, 0)
+  following_data_points <- starting_data_points - 1
   separating_data_points <- round(separating_hours / resolution, 0)
 
 
-  d <- dataFrame[,-1]
+  d <- dataFrame[,-grep(pattern = "posixDateTime", x = colnames(dataFrame))]
   v <- apply(d, 2, function(data_vector){
-    possible_starts <- which(data_vector < threshold)
 
-    # criteria 1: the following value needs to be below threshold, too
-    following <- sapply(1:(starting_data_points - 1), function(i){
-      (data_vector[possible_starts + i] < threshold)
-    })
+    if(dev_type == "elt"){
+      possible_starts <- which(data_vector <= threshold)
 
-    events <- if(length(unlist(following))){
-      if(sum(unlist(following), na.rm = T)){
+      # criteria 1: the following values, depending on deficiency_hours, need to
+      # be below threshold, too
+      following <- sapply(1:following_data_points, function(i){
+        (data_vector[possible_starts + i] <= threshold)
+      })
+    } else if(dev_type == "egt"){
+      possible_starts <- which(data_vector >= threshold)
+
+      # criteria 1: the following values, depending on deficiency_hours, need to
+      # be below threshold, too
+      following <- sapply(1:following_data_points, function(i){
+        (data_vector[possible_starts + i] >= threshold)
+      })
+    }
+
+    event_starts <- if(length(unlist(following)) > 0L){
+      if(sum(unlist(following), na.rm = T) > 0L){
         possible_starts[apply(following, MARGIN = 1, all)]
       }
     }
-
+    events <- sort(unique(c(event_starts, event_starts + following_data_points)))
 
     if(length(events) > 0){
       # criteria 2: events within a defined seperation step are aggregated as one
@@ -76,12 +93,13 @@ critical_events <- function(
         separated <- c(TRUE)
         if(nrow(a) > 1){
           for(r in 2:nrow(a)){
-            separated <- c(
-              separated,
-              sum(
-                data_vector[a$tEnd[r - 1]:a$tBeg[r]] >=
-                  recovery_value,
-                na.rm = TRUE) > 0)
+            recovering <- if(dev_type == "elt"){
+              data_vector[a$tEnd[r - 1]:a$tBeg[r]] >= recovery_value
+            } else if(dev_type == "egt"){
+              data_vector[a$tEnd[r - 1]:a$tBeg[r]] <= recovery_value
+            }
+            recovered <- sum(recovering,na.rm = TRUE) > 0L
+            separated <- c(separated,recovered)
           }
         }
         a$start <- separated
@@ -89,9 +107,13 @@ critical_events <- function(
         a$end[c((which(a$start) - 1)[2:sum(a$start)], nrow(a))] <- TRUE
 
         if(return_event_positions){
-          data.frame(lapply(a, function(x){dates[x]}))
+          data.frame(lapply(
+            a[a$end == TRUE, c("tBeg", "tEnd")],
+            function(x){
+              dates[x]
+            }))
         }  else {
-          sum(a$event)
+          sum(a$end)
         }
       } else {
         if(return_event_positions){
