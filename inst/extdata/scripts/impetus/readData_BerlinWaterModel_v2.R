960efeefa591272a945656ed5c9bc714f1a63035
daniel-wicke <daniel.wicke@kompetenz-wasser.de>
2025-10-27 17:31:48 +0100

    latest version

--- inst/extdata/scripts/impetus/readData_BerlinWaterModel_v2.R
+++ inst/extdata/scripts/impetus/readData_BerlinWaterModel_v2.R
@@ -1,11 +1,13 @@
 # Documentation of aggregation and visualization of Qsim results
 
 project_path <-
-  "Y:/iGB/Projects/IMPETUS/"
+#  "Y:/iGB/Projects/IMPETUS/"
 # "C:/Users/dwicke/Documents/work/IMPETUS"
+  "C:/Users/dwicke/Documents/R/Github"
 
-data_path <- "Work-packages/WP4_Demonstration_KWB/CS-Berlin/04_Modelling/OGewaesser/BerlinWaterModel/Ergebnisse"
-file_name <- "qsimVis_input_days_test_250905.csv"
+#data_path <- "Work-packages/WP4_Demonstration_KWB/CS-Berlin/04_Modelling/OGewaesser/BerlinWaterModel/Ergebnisse"
+data_path <- "kwb.BerlinWaterModel"
+file_name <- "qsimVis_input_days_Val_S0.csv"
 
 # find out about column names --------------------------------------------------
 colNames <- read.csv(
@@ -16,7 +18,7 @@ print(colNames)
 # load and prepare qsim data
 df_in <- qsimVis::QSIM_prepare(
   qsim_output_file = file.path(project_path, data_path, file_name),
-  parameter_name = "tracer.wwtp",
+  parameter_name = "Valsartan.g.m3", # "tracer.wwtp", "tracer.rain"
   date_column_name = "Datum",
   id_column_name = "GewaesserId",
   km_column_name = "Km",
@@ -25,8 +27,8 @@ df_in <- qsimVis::QSIM_prepare(
 )
 
 # Aggregate data
-df_pro <- df_in$para
-unit_factor <- 1/2
+df_pro <- df_in$para  # df_in$para oder df_in$flow
+unit_factor <- 1   # Umrechnungsfaktor, z.B. g/m3 in µg/L
 df_pro[,-1] <- df_pro[,-1] * unit_factor
 reference_vector <- rep(0, nrow(df_pro))
 
@@ -36,7 +38,7 @@ output <- list(
       dataFrame = df_pro,
       thresholds = c(5, 10, 15, 20, 40, 60)/100, # Anzahl flexibel
       # thresholds = c(0,10, 20, 40, 60, 80)/100,
-      dev_type = "egt", # auch "elt" = equal or lower than möglich
+      dev_type = "egt", # "elt" = equal or lower than möglich, "egt", "gt", "lt"
       relative = TRUE), # If TRUE --> relative values in %
   "adv_deviation" =
     qsimVis::adverse_deviation_from_reference(
@@ -63,31 +65,44 @@ output <- list(
 head(output$adv_deviation)
 head(output$def_hours)
 head(output$stats)
-head(output$flow_mean) # der Flow ist überall immer 19.37.
+head(output$flow_mean)
 # es gibt keinen CVK
 
 output_table <-
   "adv_deviation"
+  # "stats"
   # "def_hours"
 
-if(output_table == "adv_deviation"){
-  output_column <- "adverse_dev"
-  classBreaks <- c(0, 0.05, 0.1, 0.15, 0.25, 0.5, 1)
-  colorVector <- NULL # -> MisaColor
-  LegendTitle <- "Durchschnittliger Abwassergehalt"
-}
+
 if(output_table == "def_hours"){
   output_column <- "above_0.2"
   classBreaks <- c(0, 5, 10, 15, 25, 50, 100)
+#  classBreaks <- c(seq(0,50, 5), seq(60,100,10))
   colorVector <- NULL # -> MisaColor
   LegendTitle <- "Anteil mit mehr als 20% Abwasser in %"
 }
 
 if(output_table == "def_hours"){
-  output_column <- "above_0.2"
-  classBreaks <- c(seq(0,50, 5), seq(60,100,10))
-  colorVector <- c("blue", "green", "yellow", "orange", "darkred")
-  LegendTitle <- "Anteil mit mehr als 20% Abwasser in %"
+  output_column <- "above_0.1"
+  classBreaks <- c(0, 5, 10, 220, 33, 50, 100)
+  colorVector <- NULL # -> MisaColor
+  LegendTitle <- "Zeitanteil mit mehr als 10% Regenabfluss 2002-2022 [%]"
+}
+
+# Durchfluss < 0
+if(output_table == "def_hours"){
+  output_column <- "below_0"
+  classBreaks <- c(0, 1, 10, 15, 25, 50, 75, 100)
+  colorVector <- c("dodgerblue4", "yellow", "orange", "darkorange3", "red", "red3", "darkred")
+  LegendTitle <- "Zeitanteil mit Durchfluss<0 in 2019 [%]"
+}
+
+# Valsartansäure
+if(output_table == "stats"){
+  output_column <- "mean"
+  classBreaks <- c(0, 0.25, 0.5, 1, 2.5, 4, 6)
+  colorVector <- NULL
+  LegendTitle <- "Konzentration Valsartansäure 2019 [µg/L]"
 }
 
 # Combine river stretch and simulations data
